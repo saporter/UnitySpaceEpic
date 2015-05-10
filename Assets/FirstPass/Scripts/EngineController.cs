cd3ae4b5d4aae6de31a67e25eb36bd3c7b1c9612
saporter_mac <scott.porter.ii@gmail.com>
2015-05-10 12:56:16 -0500

    Finished removing FirstPass

--- Assets/FirstPass/Scripts/EngineController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EngineController : MonoBehaviour {
-	public static float RotationCost = 1f / 90f;
-
-	public float EngineCapacity = 10f;
-	public float RotationSpeed = 100f;
-}

17ea526c7422a7580a960f2200b7d230a24d0531
Scott Porter <scott.porter.ii@gmail.com>
2015-05-13 22:17:30 -0500

    Added sunprefab from Activeden.net in Third Party Assets.  No Third Party will be added in repository from now on.

---
+++ Assets/Scripts/RefreshPlanetsOnAwake.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefreshPlanetsOnAwake : MonoBehaviour {
+
+	void Awake()
+	{
+		this.gameObject.GetComponent<F3DSun>().Planets = FindObjectsOfType<F3DPlanet>();
+	}
+}

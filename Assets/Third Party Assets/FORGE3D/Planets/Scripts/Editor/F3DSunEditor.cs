d9e9d539b00e870df3d68a88b9a20bc4d4f45782
Scott Porter <scott.porter.ii@gmail.com>
2015-05-15 16:34:01 -0500

    Added scripts folder from FORGE3D into repository before major edits

---
+++ Assets/Third Party Assets/FORGE3D/Planets/Scripts/Editor/F3DSunEditor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(F3DSun))]
+public class F3DSunEditor : Editor
+{
+    [MenuItem("FORGE3D/Planets/Add F3DSun")]
+    static void AddSunScript()
+    {
+        Selection.activeGameObject.AddComponent<F3DSun>();
+    }
+
+    [MenuItem("FORGE3D/Planets/Add F3DSun", true, 0)]
+    static bool ValidateSunScript()
+    {
+        if (Selection.activeGameObject != null && !Selection.activeGameObject.GetComponent<F3DSun>())
+            return true;
+        else return false;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        F3DSun myTarget = (F3DSun)target;
+
+        EditorGUILayout.BeginVertical(); // BEGIN
+
+        //
+        GUIStyle smallFont = new GUIStyle();
+        smallFont.fontSize = 9;
+        smallFont.wordWrap = true;
+
+        smallFont.normal.textColor = new Color(0.7f, 0.7f, 0.7f);
+
+        GUIStyle headerFont = new GUIStyle();
+        headerFont.fontSize = 11;
+        headerFont.fontStyle = FontStyle.Bold;
+        headerFont.normal.textColor = new Color(0.75f, 0.75f, 0.75f);
+
+        GUIStyle subHeaderFont = new GUIStyle();
+        subHeaderFont.fontSize = 10;
+        subHeaderFont.fontStyle = FontStyle.Bold;
+        subHeaderFont.margin = new RectOffset(1, 0, 0, 0);
+        subHeaderFont.padding = new RectOffset(1, 0, 3, 0);
+        subHeaderFont.normal.textColor = new Color(0.70f, 0.70f, 0.70f);
+        //
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Sun lighting:", headerFont);
+
+        EditorGUILayout.BeginVertical("Box");
+        EditorGUILayout.BeginVertical("Box");
+        
+        if(myTarget.Planets != null)
+            EditorGUILayout.LabelField("Soft shadowed objects in scene: " + myTarget.Planets.Length, smallFont);
+        else
+            EditorGUILayout.LabelField("No objects. Try to refresh.", smallFont);
+
+        myTarget.PlanetLayer = EditorGUILayout.LayerField("Planet layer:", myTarget.PlanetLayer);
+
+        if (GUILayout.Button("Refresh"))
+        {
+            myTarget.Planets = FindObjectsOfType<F3DPlanet>();
+            Debug.Log("F3DSun: Updated " + myTarget.Planets.Length + " objects.");
+        }
+        EditorGUILayout.EndVertical();
+
+        myTarget.EnableSoftShadow = EditorGUILayout.Toggle("Enable soft shadows", myTarget.EnableSoftShadow);        
+        myTarget.shadowStrength = EditorGUILayout.Slider("Shadow strength:", myTarget.shadowStrength, 0.0f, 1.0f);
+        myTarget.radius = EditorGUILayout.Slider("Sun radius:", myTarget.radius, 0.1f, 5f);
+
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndVertical(); // END
+
+        EditorUtility.SetDirty(myTarget);
+    }	
+}
---
+++ Assets/Third Party Assets/FORGE3D/Planets/Scripts/F3DSun.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class F3DSun : MonoBehaviour
+{
+    public F3DPlanet[] Planets;
+
+    public float radius;
+    public float shadowStrength;
+
+    int ShadowCasters = 20;
+
+    public bool EnableSoftShadow;
+
+    public float RotationRate;
+
+    public int PlanetLayer;
+
+    public Color sunColor;
+
+    int sunRadiusRef, sunPosRef, shadowStrengthRef;
+    int[] shadowCasterRef;
+
+    Vector4 dummyShadowPos = new Vector4(20000f, 20000f, 20000f, 0f);
+    Vector4[] shadowCasters;
+
+    // Use this for initialization
+    void Awake()
+    {
+        shadowCasterRef = new int[ShadowCasters];
+        sunRadiusRef = Shader.PropertyToID("_SunRadius");
+        sunPosRef = Shader.PropertyToID("_SunPos");
+        shadowStrengthRef = Shader.PropertyToID("_ShadowStrength");
+
+        for (int n = 1; n < ShadowCasters + 1; n++)
+            shadowCasterRef[n - 1] = Shader.PropertyToID("_ShadowCasterPos_" + n.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0f, RotationRate * Time.deltaTime, 0f);
+
+        if (Planets != null)
+        {
+            for (int i = 0; i < Planets.Length; i++)
+            {
+                if (Planets[i] == null)
+                {
+                    Debug.LogWarning("F3DSun : Planet script has been removed from one of the objects in the scene. Please refresh.");
+                    Planets = null;
+                    return;
+                }
+
+                if (EnableSoftShadow)
+                {
+                    RaycastHit[] hits = Physics.SphereCastAll(transform.position, Mathf.Max(Planets[i].transform.localScale.x, radius), Planets[i].transform.position - transform.position, Vector3.Distance(transform.position, Planets[i].transform.position), 1 << PlanetLayer);
+
+                    shadowCasters = new Vector4[ShadowCasters];
+
+                    for (int n = 0; n < shadowCasters.Length; n++)
+                        shadowCasters[n] = dummyShadowPos;
+
+                    for (int n = 0; n < hits.Length && n < ShadowCasters; n++)
+                    {
+                        if (hits[n].transform != Planets[i].transform)
+                        {
+                            shadowCasters[n] = hits[n].collider.transform.position;
+                            shadowCasters[n].w = hits[n].collider.transform.localScale.x / 2.0f;
+                        }
+                        else
+                            shadowCasters[n] = dummyShadowPos;
+                    }
+                }
+                
+                Renderer[] planetRenderers = Planets[i].GetComponentsInChildren<Renderer>();
+
+                for (int m = 0; m < planetRenderers.Length; m++)
+                {
+                    planetRenderers[m].sharedMaterial.SetVector(sunPosRef, transform.position);                    
+
+                    if (EnableSoftShadow)
+                    {
+                        planetRenderers[m].sharedMaterial.SetFloat(sunRadiusRef, radius);
+
+                        for (int n = 0; n < ShadowCasters; n++)
+                        {
+                            planetRenderers[m].sharedMaterial.SetVector(shadowCasterRef[n], shadowCasters[n]);
+                            planetRenderers[m].sharedMaterial.SetFloat(shadowStrengthRef, shadowStrength);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, transform.localScale.x * 0.55f);
+    }
+}
